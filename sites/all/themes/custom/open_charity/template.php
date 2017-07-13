cbfea0a35c5f6e820c8ecc19157dbf05a3336cdf
neerajkcs <neerajkcsharma@gmail.com>
2017-07-13 18:20:52 +0530

    Theme initial work

---
+++ sites/all/themes/custom/open_charity/template.php
@@ -0,0 +1,14 @@
+<?php
+
+function open_charity_preprocess_page(&$variables) {
+  // Add information about the number of sidebars.
+  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $variables['content_column_class'] = ' class="col-6"';
+  }
+  elseif (!empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
+    $variables['content_column_class'] = ' class="col-9"';
+  }
+  else {
+    $variables['content_column_class'] = ' class="col-12"';
+  }
+}
